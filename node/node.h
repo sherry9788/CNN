e6cffca8ca38df05aa659ee4ec296432123cefa5
sherry9788 <sherry9788@g.ucla.edu>
2017-01-21 08:05:30 -0800

    Update node.h

--- node/node.h
+++ node/node.h
@@ -22,24 +22,32 @@ public:
     node(ITER1 prev_edges_beg, ITER1 prev_edges_end,
             ITER2 next_edges_beg, ITER2 next_edges_end);
 
-
+    // non-linear process the signal it receives
     void activate(double n_input);
+    // get the output of activation
     double get_activation() const;
 
+    // add an edge to this node
     void add_prev_edge(ptr_edge_t n_prev_edge);
+    // add an edge from this node
     void add_next_edge(ptr_edge_t n_next_edge);
 
+    // get the non-linear function
     nonlinear_function &get_op();
     nonlinear_function get_op() const;
 
+    // get "delta", will be used in backward propagation
     double &get_delta();
     double get_delta() const;
 
+    // get the input signal
     double get_input() const;
 
+    // get the edge to this node
     vec_edge_t &get_prev_edge();
     vec_edge_t get_prev_edge() const;
 
+    // get the edge from this node
     vec_edge_t &get_next_edge();
     vec_edge_t get_next_edge() const;
 
